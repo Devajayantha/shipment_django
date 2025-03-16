137647ec84c37a0521e049b0aa2ecfc547410077
Devajayantha <devajayantha888@gmail.com>
2025-03-16 16:20:40 +0800

    add base templade for index and create shipment

--- shipment/urls.py
+++ shipment/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="shipment_list"),
+    path("create/", views.create, name="shipment_create"),
 ]
--- shipment/views.py
+++ shipment/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html', context={})
+
+def create(request):
+    return render(request, 'create.html', context={})
